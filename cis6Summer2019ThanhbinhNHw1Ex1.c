90540fb966c381b091e5784dbfb13f78a2fe3527
NguyenVoT22 <53206387+NguyenVoT22@users.noreply.github.com>
2019-09-06 21:29:59 -0700

    Add files via upload

---
+++ cis6Summer2019ThanhbinhNHw1Ex1.c
@@ -0,0 +1,65 @@
+/**
+ * Program Name: cis6Summer2019ThanhbinhNHw1Ex1.c
+ * Discussion:   Hw #1 Ex #1
+ * Written By:   Thanhbinh N
+ * Date:         2019/06/24
+ */
+
+#include <stdio.h>
+
+int main() {
+
+	printf("CIS 6 - Introduction to Programming  (Using C)\n"
+		"Laney College\n"
+		"Thanhbinh N\n\n"
+		"Assignment Information --\n"
+		"  Assignment Number:  Homework #1\n"
+		"                      Coding Assignment -- Exercise #1\n"
+		"  Written by:         Thanhbinh N\n"
+		"  Submitted Date:     2019/06/24\n\n");
+
+	printf("TTTTTTTTT  N        N\n"
+		"    T      NN       N\n"
+		"    T      N N      N\n"
+		"    T      N  N     N\n"
+		"    T      N   N    N\n"
+		"    T      N    N   N\n"
+		"    T      N     N  N\n"
+		"    T      N      N N\n"
+		"    T      N       NN\n\n\n");
+	
+	printf("There are 17 T's, and 26 N's.\n\n"
+	    "There are 43 characters in total.\n");
+	return 0;
+}
+
+/* PROGRAM_OUTPUT
+CIS 6 - Introduction to Programming  (Using C)
+Laney College
+Thanhbinh N
+
+Assignment Information --
+  Assignment Number:  Homework #1
+                      Coding Assignment -- Exercise #1
+  Written by:         Thanhbinh N
+  Submitted Date:     2019/06/24
+
+TTTTTTTTT  N        N
+    T      NN       N
+    T      N N      N
+    T      N  N     N
+    T      N   N    N
+    T      N    N   N
+    T      N     N  N
+    T      N      N N
+    T      N       NN
+
+
+There are 17 T's, and 26 N's.
+
+There are 43 characters in total.
+ */
+
+/* YOUR_Logic Issues_Code Issues, Output Issues_COMMENTS
+No comments!
+ */
